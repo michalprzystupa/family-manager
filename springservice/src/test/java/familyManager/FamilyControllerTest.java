385fe3ec905b1ed245ddfbb9e3973142d95d3772
Michal Przystupa <mprzystupa@wp.pl>
2018-06-27 19:08:36 +0200

    Refator test

--- springservice/src/test/java/familyManager/FamilyControllerTest.java
+++ springservice/src/test/java/familyManager/FamilyControllerTest.java
@@ -12,13 +12,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Arrays;
 
@@ -31,84 +31,73 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class FamilyControllerTest {
-    private static final int FIRST_CREATED_FAMILY_ID = 1;
     private final String FAMILIES_URL = "http://localhost:8081/families";
-    private final String SELECT_COUNT_FROM_FAMILY_QUERY = "SELECT count(*) FROM FAMILY";
 
     @Autowired
     private WebApplicationContext context;
 
-    @Autowired
-    private FamilyController subject;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     private MockMvc mockMvc;
+    private Gson jsonConverter;
 
     @Before
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-    }
-
-    @Test
-    public void shouldCreateNewFamily() throws Exception {
-        assertEquals(0, getFamilyCount());
-
-        MockHttpServletResponse response = mockMvc.perform(post(FAMILIES_URL))
-                .andExpect(status().isCreated())
-                .andDo(print())
-                .andReturn()
-                .getResponse();
-        long familyId = Long.decode(response.getContentAsString());
-
-        assertEquals(1, getFamilyCount());
-        assertEquals(FIRST_CREATED_FAMILY_ID, familyId);
+        jsonConverter = Converters.registerLocalDate(new GsonBuilder()).create();
     }
 
     @Test
     public void shouldAddFamilyMembersAndGetFamily() throws Exception {
-        Gson gson = Converters.registerLocalDate(new GsonBuilder()).create();
+        Father father = readFromFile("father.json", Father.class);
+        Child[] children = readFromFile("children.json", Child[].class);
+        Family family = new Family(father, Arrays.asList(children));
 
-        //        create family
+        long familyId = createFamily();
+
+        createFather(familyId, father);
+
+        createChild(familyId, children[0]);
+        createChild(familyId, children[1]);
+
+        String familyAsJson = readFamilyAsJson(familyId);
+
+        assertEquals(jsonConverter.toJson(family), familyAsJson);
+    }
+
+    private <T> T readFromFile(String fileName, Class<T> classOfT) throws FileNotFoundException {
+        return jsonConverter.fromJson(new FileReader(getClass().getClassLoader().getResource(fileName).getFile()), classOfT);
+    }
+
+    private long createFamily() throws Exception {
         MockHttpServletResponse createFamilyResponse = mockMvc.perform(post(FAMILIES_URL))
                 .andExpect(status().isCreated())
                 .andDo(print())
                 .andReturn()
                 .getResponse();
-        long familyId = Long.decode(createFamilyResponse.getContentAsString());
+        return Long.decode(createFamilyResponse.getContentAsString());
+    }
 
-        //        add father
-        Father father = gson.fromJson(new FileReader(getClass().getClassLoader().getResource("father.json").getFile()), Father.class);
+    private void createFather(long familyId, Father father) throws Exception {
         mockMvc.perform(post(FAMILIES_URL + "/" + familyId + "/father")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(gson.toJson(father)))
+                .content(jsonConverter.toJson(father)))
                 .andExpect(status().isCreated())
                 .andDo(print());
-//        add children
-        Child[] children = gson.fromJson(new FileReader(getClass().getClassLoader().getResource("children.json").getFile()), Child[].class);
-        mockMvc.perform(post(FAMILIES_URL + "/" + familyId + "/children")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(gson.toJson(children[0])))
-                .andExpect(status().isCreated())
-                .andDo(print());
-        mockMvc.perform(post(FAMILIES_URL + "/" + familyId + "/children")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(gson.toJson(children[1])))
-                .andExpect(status().isCreated())
-                .andDo(print());
+    }
 
-//        read family
-        MockHttpServletResponse getFamilyResponse = mockMvc.perform(get(FAMILIES_URL + "/" + familyId))
+    private void createChild(long familyId, Child child) throws Exception {
+        mockMvc.perform(post(FAMILIES_URL + "/" + familyId + "/children")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonConverter.toJson(child)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
+
+    private String readFamilyAsJson(long familyId) throws Exception {
+        return mockMvc.perform(get(FAMILIES_URL + "/" + familyId))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andReturn()
-                .getResponse();
-
-        assertEquals(gson.toJson(new Family(father, Arrays.asList(children))), getFamilyResponse.getContentAsString());
-    }
-
-    private int getFamilyCount() {
-        return jdbcTemplate.queryForObject(SELECT_COUNT_FROM_FAMILY_QUERY, Integer.class);
+                .getResponse()
+                .getContentAsString();
     }
 }

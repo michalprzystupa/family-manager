0632c4aac116abb165680a162565d89fb53fa592
Michal Przystupa <mprzystupa@wp.pl>
2018-06-29 00:53:10 +0200

    Add routing module

--- angular-service/src/app/app.module.ts
+++ angular-service/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
     NavigationBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
